3c681fbd0dd5cd186270dcbd57ca88e73b747701
sarvagyadabas <sarvagyadabas@gmail.com>
2024-08-21 14:19:02 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //Treat all js code as
+
+// alert(3+3) we are using nodejs not browser
+
+// number =>2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+let name="hitesh"
+let age=14
+
+console.log(typeof name);
+console.log(typeof age);

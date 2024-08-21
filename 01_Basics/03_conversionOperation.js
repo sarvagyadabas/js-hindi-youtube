b439fcde2495def018978e541f8d277b23f57e29
sarvagyadabas <sarvagyadabas@gmail.com>
2024-08-21 17:51:33 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -23,3 +23,26 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+/////////////*************OPERATIONS *******************************////////////////////////////
+
+let value = 3
+let negvalue = -value
+
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);

108369e407ff6019eca90837050cbde95e80e111
sarvagyadabas <sarvagyadabas@gmail.com>
2024-08-23 19:11:26 +0000

    numbers,maths and dates

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,50 @@
+// // const score = 400
+// // console.log(score);
+
+// // const balance = new Number(100)
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+
+// // console.log(balance.toFixed(2));
+
+
+// const otherNumber= 123.11323
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ********************************** MATHS
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(-4.6));
+// console.log(Math.ceil(-4.6));
+// console.log(Math.floor(-4.6));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.random());// this always shows a value greater than 0 and less than 1
+// console.log(Math.floor(Math.random()*10) +1);
+
+// const min=10
+// const max=10
+
+// console.log(Math.random() * (max-min+1)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,33 @@
+// // //Dates
+
+// // //let myDate = new Date()
+
+
+// // // console.log(myDate.toString());
+// // // console.log(myDate.toDateString());
+// // // console.log(myDate.toISOString());
+// // // console.log(myDate.toJSON());
+// // // console.log(myDate.toLocaleDateString());
+// // // console.log(myDate.toLocaleString());
+// // // console.log(typeof myDate);
+
+// let myCreatedDate = new Date("08-24-2023")
+// // console.log(myCreatedDate.toDateString);
+
+// let myTimeStamp = Date.now()
+// // console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+// //To convert to seconds
+// console.log(Math.floor((Date.now())/1000));
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+
+// console.log(`The date today is ${newDate} and the day today is ${newDate.getDay()}.`);
+
+newDate.toLocaleString('default',{
+    weekday:"long" 
+})
+

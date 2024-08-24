12dde87d8b0942c28efb4a3dee115c0596255922
sarvagyadabas <sarvagyadabas@gmail.com>
2024-08-24 16:44:31 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,39 @@
+//ARRAYS
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes=["shaktiman","nagraj"]
+const myArr2 = new Array(1,2,3,4,5)
+
+//console.log(myArr[1]);
+
+// ARRAY METHODS
+
+// myArr.push(6)
+// myArr.push(9)// adds the element in the bracket at the end of the array
+// console.log( myArr);
+ 
+// myArr.pop()// removes the last value of an array
+// myArr.unshift(9)// adds the value in bracket in the starting of an array
+// myArr.shift()// removes the first value of an array
+
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()// makes array into string 
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// console.log("A",myArr);
+
+// const myn1=myArr.slice(1,3)//displays the first and second index element beacuase 3 is not included
+
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn2=myArr.splice(1,3)//removes the elements from index 1,2 and 3 
+// console.log("C",myArr);
+// console.log(myn2);
+
+
